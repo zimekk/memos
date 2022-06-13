4d8fb9d0639c2e68fe1a55a8eac9280ceaf94ad6
Marcin Makarewicz <marcin@makarewicz.eu>
2022-06-14 00:16:22 +0200

    feat: native ios modal with page sheet presentation style

--- Components/MyModal.ios.tsx
+++ Components/MyModal.ios.tsx
@@ -1,53 +1,32 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Button, Modal, StyleSheet, Text, View } from "react-native";
-import { FixedContent } from "./modals/FixedContent";
 
 export function MyModal() {
-  const modal = useRef(null);
   const [open, setOpen] = useState(false);
 
   return (
-    <View style={styles.container}>
+    <>
       <Button
         title={open ? "Hide modal" : "Show modal"}
-        onPress={() =>
-          setOpen((open) => {
-            if (open) {
-              modal.current.close();
-            } else {
-              modal.current.open();
-            }
-            return !open;
-          })
-        }
+        onPress={() => setOpen((open) => !open)}
       />
-      <FixedContent ref={modal} />
-      <Modal animationType="slide" visible={open}>
+      <Modal animationType="slide" presentationStyle="pageSheet" visible={open}>
         <View style={styles.modal}>
+          <Text>MyModal!</Text>
           <Button
             title={open ? "Hide modal" : "Show modal"}
             onPress={() => setOpen((open) => !open)}
           />
         </View>
       </Modal>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#ede3f2",
-    padding: 100,
-  },
   modal: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#f7021a",
     padding: 100,
-  },
-  text: {
-    color: "#3f2949",
-    marginTop: 10,
   },
 });
